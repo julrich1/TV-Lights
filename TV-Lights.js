44793dae305970d9ca92906bf4706acfd3879667
Jacob Ulrich <julrich@gmail.com>
2017-08-27 11:17:28 -0700

    Initial commit

---
+++ TV-Lights.js
@@ -0,0 +1,44 @@
+const mdns = require("mdns");
+const Hue = require("philips-hue");
+
+const hue = new Hue;
+
+const ccTargetName = "Living Room";
+const lightState = {bri: 255, sat: 255, hue: 0}; //Red Full Brightness
+
+hue.bridge = "192.168.1.212";  // from hue.getBridges
+hue.username = require("./HUE_USERNAME"); // from hue.auth
+
+setInterval(() => {
+  const browser = mdns.createBrowser(mdns.tcp("googlecast"));
+  
+  browser.start();
+  
+  browser.on("serviceUp", function(service) {
+    if (service.txtRecord.fn === ccTargetName) {
+      console.log("Found target device");
+
+      if (service.txtRecord.rs) {
+
+        console.log("Turning lights on");
+        Promise.all([hue.light(7).on(), hue.light(8).on(), hue.light(7).setState(lightState), hue.light(8).setState(lightState)])
+          .then(() => {
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      else {
+        console.log("Turning lights off");
+        Promise.all([hue.light(7).off(), hue.light(8).off()])
+          .then(() => {
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+
+    browser.stop();
+  });
+}, 5000);
